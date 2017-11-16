75dde93eaf74bdd1c07999fd4c797442373b10de
Andrew SaLoutos <andrewsaloutos2018@u.northwestern.edu>
2017-11-15 21:32:57 -0600

    testing epm magnets

--- V1/shakecode/shake.c
+++ V1/shakecode/shake.c
@@ -9,18 +9,61 @@
 
 #define tbi(x,y) x ^= _BV(y) //toggle bit - using bitwise XOR operator
 
+void set_up_led(int);
+void set_out_led(int);
+
 int main(void)
 {
 	DDRB=0xff; // PortB as output
 	PORTB=0x00; // All pins low
 	
 	unsigned int i;
+	int state = 0;
+
+	set_out_led(1);
+	set_up_led(1);
+	_delay_ms(1000);
+	set_up_led(0);	
+
 	while(1==1) // infinite loop sending one bits
 	{
 		for(i=0;i<65535;i++);
 		for(i=0;i<65535;i++);		
-		tbi(PORTB,PB0); // toggle	
+		//PORTB |= (1<<PB0); // always set to 1
+		tbi(PORTB,PB0); // toggle
+		set_out_led(state);
+		if (state==1) { state=0; }
+		if (state==0) { state=1; }	
 	}		
+}
+
+void set_out_led(int state)
+{
+	DDRD |= (1<<3);
+	PORTD |= (1<<3);
+	if(state==1)
+	{
+		PIND |= (1<<3);
+	}
+	else
+	{
+		PIND &= ~(1<<3);
+	}
+
+}
+void set_up_led(int state)
+{
+	DDRD |= (1<<4);
+	PORTD |= (1<<4);
+	if(state==1)
+	{
+		PIND |= (1<<4);
+	}
+	else
+	{
+		PIND &= ~(1<<4);
+	}
+
 }
 
 
